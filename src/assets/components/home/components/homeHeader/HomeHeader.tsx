edc568d106098435ab1c9eba55662d97a994cda8
Daniel Martinez Duque <danielopq@gmail.com>
2024-12-17 01:41:04 +0000

    HomeHeader.tsx - background, text and logo added

--- src/assets/components/home/components/homeHeader/HomeHeader.tsx
+++ src/assets/components/home/components/homeHeader/HomeHeader.tsx
@@ -1,8 +1,16 @@
 import './homeHeader.css';
 
-const HomeHeader:React.FC = ()=>{
-    return(
-        <header id="homeHeader"></header>
+const HomeHeader: React.FC = () => {
+    return (
+        <header id="homeHeader">
+            <div>
+                <h1>Exquisite dining since 1989</h1>
+                <p className='mediumText'>
+                    Experience our seasonal menu in beautiful country surroundings.
+                    Eat the freshest produce from the comfort of our farmhouse.
+                </p>
+            </div>
+        </header>
     )
 }
 export default HomeHeader;
