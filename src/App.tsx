919c33b48cb7cf4dcf509718e3443842bf4c325f
Daniel Martinez Duque <danielopq@gmail.com>
2024-12-17 00:28:51 +0000

    Home.tsx - created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css'
+import Home from './assets/components/home/Home'
 
 function App() {
   return (
     <>
+      <Home />
     </>
   )
 }
---
+++ src/assets/components/home/Home.tsx
@@ -0,0 +1,10 @@
+import './home.css';
+
+const Home:React.FC = ()=>{
+    return(
+        <main id="home">
+            Home page
+        </main>
+    )
+}
+export default Home;
