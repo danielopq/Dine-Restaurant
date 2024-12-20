15a61adf57dfe34e48499983e1529dc69d2f409c
Daniel Martinez Duque <danielopq@gmail.com>
2024-12-20 18:00:07 +0000

    EnjoyablePlace.tsx - done

--- src/assets/components/home/components/enjoyablePlace/EnjoyablePlace.tsx
+++ src/assets/components/home/components/enjoyablePlace/EnjoyablePlace.tsx
@@ -1,10 +1,23 @@
 import './enjoyablePlace.css';
 
+
+/**
+ * Represents the 'EnjoyablePlace' section of the website.
+ * 
+ * @returns {JSX.Element} - The 'EnjoyablePlace' component displaying the section content.
+ */
 const EnjoyablePlace:React.FC = () =>{
     return(
         <section id="enjoyablePlace">
             <div id="enjoyablePlace-pic"></div>
-            <div id="enjoyablePlace-content"></div>
+            <div id="enjoyablePlace-content">
+                <h2>Enjoyable place for all the family</h2>
+                <p className='mediumText'>
+                    Our relaxed surroundings make dining with us a great 
+                    experience for everyone. We can even arrange a tour 
+                    of the farm before your meal.
+                </p>
+            </div>
         </section>
     )
 }
