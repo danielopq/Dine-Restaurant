5a96e0c8b4058bddc4cde7ce80b65616f7d0b18f
Daniel Martinez Duque <danielopq@gmail.com>
2024-12-20 19:00:21 +0000

    LocallySourcedFood.tsx

--- src/assets/components/home/components/locallySourcedFood/LocallySourcedFood.tsx
+++ src/assets/components/home/components/locallySourcedFood/LocallySourcedFood.tsx
@@ -1,5 +1,10 @@
 import './locallySourcedFood.css';
 
+/**
+ * Renders the section "locallySourcedFood";
+ * 
+ * @returns {JSX.Element} - A React component displaying locally sourced food information.
+ */
 const LocallySourcedFood: React.FC = () => {
     return (
         <section id="locallySourcedFood">
@@ -10,7 +15,9 @@ const LocallySourcedFood: React.FC = () => {
                     So you can be sure that you’re eating the freshest, most sustainable food.
                 </p>
             </div>
-            <div id="lsf-Pic"></div>
+            <div id="lsf-Pic">
+                <div></div>
+            </div>
         </section>
     )
 }
