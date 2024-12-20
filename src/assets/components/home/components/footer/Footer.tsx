defa0cb2952ca1b77b90dabcc706b7fcfac97702
Daniel Martinez Duque <danielopq@gmail.com>
2024-12-20 22:17:40 +0000

    Footer.tsx - created

--- src/assets/components/home/components/footer/Footer.tsx
+++ src/assets/components/home/components/footer/Footer.tsx
@@ -3,7 +3,21 @@ import './footer.css';
 const Footer:React.FC = () =>{
     return(
         <footer>
-
+            <div id="footerTop">
+                <h2>Ready to make a reservation?</h2>
+            </div>
+            <div id="footerBottom">
+                <div>
+                    <p>Marthwaite, Sedbergh</p>
+                    <p>Cumbria</p>
+                    <p>+00 44 123 4567</p>
+                </div>
+                <div>
+                    <p>OPEN TIMES</p>
+                    <p>MON - FRI: 09:00 AM - 10:00 PM</p>
+                    <p>SAT - SUN: 09:00 AM - 11:30 PM</p>
+                </div>
+            </div>
         </footer>
     )
 }
