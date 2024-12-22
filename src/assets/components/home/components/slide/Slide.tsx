767ce2b4d11f202cd169659508f69165624b5205
Daniel Martinez Duque <danielopq@gmail.com>
2024-12-22 00:12:14 +0000

    SlidePicture.tsx - created

--- src/assets/components/home/components/slide/Slide.tsx
+++ src/assets/components/home/components/slide/Slide.tsx
@@ -1,8 +1,14 @@
 import './slide.css';
+import SlidePicture from './slidePicture/SlidePicture';
 
 const Slide:React.FC = ()=>{
     return(
-        <section id="slide"></section>
+        <section id="slide">
+            <div id="slideImages">
+                <SlidePicture/>
+            </div>
+            <div id="slide-content"></div>
+        </section>
     )
 }
 export default Slide;
---
+++ src/assets/components/home/components/slide/slidePicture/SlidePicture.tsx
@@ -0,0 +1,8 @@
+import './slidePicture.css';
+
+const SlidePicture:React.FC = ()=>{
+    return(
+        <div className='slidePicture'></div>
+    )
+}
+export default SlidePicture;
