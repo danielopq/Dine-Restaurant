e47b8abf69fb70c941d260070c19f81b5125d78f
Daniel Martinez Duque <danielopq@gmail.com>
2024-12-30 21:20:19 +0000

    ReservationsHeader.tsx - general layout

--- src/assets/components/reservations/reservationHeader/ReservationsHeader.tsx
+++ src/assets/components/reservations/reservationHeader/ReservationsHeader.tsx
@@ -1,8 +1,17 @@
 import './reservationsHeader.css';
 
-const ReservationsHeader:React.FC = () =>{
-    return(
-        <header id="reservationsHeader"></header>
+const ReservationsHeader: React.FC = () => {
+    return (
+        <header id="reservationsHeader">
+            <div id="reservations-text">
+                <h1>Reservations</h1>
+                <p className='mediumText'>
+                    We can’t wait to host you. If you have any special requirements please feel
+                    free to call on the phone number below. We’ll be happy to accommodate you.
+                </p>
+            </div>
+            <div id="reservations-form"></div>
+        </header>
     )
 }
 export default ReservationsHeader;
