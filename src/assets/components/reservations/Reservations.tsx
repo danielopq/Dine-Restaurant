70838919609450bebbfd1b12bad864d013b28a6b
Daniel Martinez Duque <danielopq@gmail.com>
2024-12-30 20:45:54 +0000

    ReservationsHeader.tsx - created

--- src/assets/components/reservations/Reservations.tsx
+++ src/assets/components/reservations/Reservations.tsx
@@ -1,9 +1,12 @@
 import './reservations.css';
+import FooterDetails from '../shared/footerDetails/footerDetails';
+import ReservationsHeader from './reservationHeader/ReservationsHeader';
 
-const Reservations:React.FC = () =>{
-    return(
+const Reservations: React.FC = () => {
+    return (
         <main id="reservations">
-
+            <ReservationsHeader />
+            <FooterDetails />
         </main>
     )
 }
---
+++ src/assets/components/reservations/reservationHeader/ReservationsHeader.tsx
@@ -0,0 +1,8 @@
+import './reservationsHeader.css';
+
+const ReservationsHeader:React.FC = () =>{
+    return(
+        <header id="reservationsHeader"></header>
+    )
+}
+export default ReservationsHeader;
