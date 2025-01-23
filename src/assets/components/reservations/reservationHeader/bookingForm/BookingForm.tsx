09de6964b22768844fad749f8e6db630495780b8
Daniel Martinez Duque <danielopq@gmail.com>
2025-01-23 14:06:57 +0000

    bookingForm.tsx - general layout

--- src/assets/components/reservations/reservationHeader/bookingForm/BookingForm.tsx
+++ src/assets/components/reservations/reservationHeader/bookingForm/BookingForm.tsx
@@ -1,8 +1,31 @@
-import './bookingForm.css'
+import './bookingForm.css';
+import DefaultButton from '../../../shared/defaultButton/DefaultButton';
 
-const BookingForm:React.FC = () =>{
-    return(
-        <div id="bookingForm"></div>
+const BookingForm: React.FC = () => {
+    return (
+        <div id="bookingForm">
+            <div className='formLine'>
+                <input id="" type='text' placeholder='Name'></input>
+                <p></p>
+            </div>
+            <div className='formLine'>
+                <input id="" type='text' placeholder='Email'></input>
+                <p></p>
+            </div>
+            <div className='formLine'>
+                <p>Pick a date</p>
+                <div></div>
+            </div>
+            <div className='formLine'>
+                <p>Pick a time</p>
+            </div>
+            <div className='formLine'>
+                <p>-</p>
+                <p>4 people</p>
+                <p>+</p>
+            </div>
+            <DefaultButton text='MAKE RESERVATION' buttonType='darkBt'/>
+        </div>
     )
 }
 export default BookingForm;
