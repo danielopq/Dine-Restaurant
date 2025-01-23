1c0ff857fb9eca189676ca00a0ac646011318194
Daniel Martinez Duque <danielopq@gmail.com>
2025-01-23 15:17:40 +0000

    BookingForm.tsx - form lines general set finished

--- src/assets/components/reservations/reservationHeader/bookingForm/BookingForm.tsx
+++ src/assets/components/reservations/reservationHeader/bookingForm/BookingForm.tsx
@@ -5,31 +5,55 @@ import { useRef } from 'react';
 const BookingForm: React.FC = () => {
     const refName = useRef<HTMLInputElement>(null);
     const refEmail = useRef<HTMLInputElement>(null);
+    const refDay = useRef<HTMLInputElement>(null);
+    const refMonth = useRef<HTMLInputElement>(null);
+    const refYear = useRef<HTMLInputElement>(null);
+    const refHour = useRef<HTMLInputElement>(null);
+    const refMinutes = useRef<HTMLInputElement>(null);
+    const refDayTime = useRef<HTMLInputElement>(null);
     const refNameError = useRef<HTMLParagraphElement>(null);
     const refEmailError = useRef<HTMLParagraphElement>(null);
+    const refDateError = useRef<HTMLParagraphElement>(null);
+    const refTimeError = useRef<HTMLParagraphElement>(null);
     return (
         <div id="bookingForm">
             <div id="nameLine" className='formLine'>
                 <input ref={refName} id="name" type='text' placeholder='Name'></input>
-                <p ref={refNameError}>This field is required</p>
+                <p ref={refNameError} className='error'></p>
             </div>
             <div id="emailLine" className='formLine'>
                 <input ref={refEmail} id="email" type='text' placeholder='Email'></input>
-                <p ref={refEmailError}>This field is required</p>
+                <p ref={refEmailError} className='error'></p>
             </div>
-            <div className='formLine'>
-                <p>Pick a date</p>
-                <div></div>
+            <div id="dateLine" className='formLine'>
+                <div>
+                    <p>Pick a date</p>
+                    <p ref={refDateError} className='error'></p>
+                </div>
+                <div>
+                    <input ref={refDay} id="day" type='text' placeholder='DD'></input>
+                    <input ref={refMonth} id="month" type='text' placeholder='MM'></input>
+                    <input ref={refYear} id="year" type='text' placeholder='YYYY'></input>
+                </div>
             </div>
-            <div className='formLine'>
-                <p>Pick a time</p>
+            <div id="timeLine" className='formLine'>
+                <div>
+                    <p>Pick a time</p>
+                    <p ref={refTimeError} className='error'></p>
+                </div>
+                <div>
+                    <input ref={refHour} id="day" type='text' placeholder='HH'></input>
+                    <input ref={refMinutes} id="month" type='text' placeholder='MM'></input>
+                    <input ref={refDayTime} id="year" type='text' placeholder='AM'></input>
+                </div>
             </div>
-            <div className='formLine'>
+
+            <div id="guestsLine" className='formLine'>
                 <p>-</p>
                 <p>4 people</p>
                 <p>+</p>
             </div>
-            <DefaultButton text='MAKE RESERVATION' buttonType='darkBt'/>
+            <DefaultButton text='MAKE RESERVATION' buttonType='darkBt' />
         </div>
     )
 }
