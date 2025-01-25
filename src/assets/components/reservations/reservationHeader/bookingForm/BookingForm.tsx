7f82ab251ea79bcc61535d5051e77c7c7c8ff3e8
Daniel Martinez Duque <danielopq@gmail.com>
2025-01-25 08:42:53 +0000

    Booking form - final layout done

--- src/assets/components/reservations/reservationHeader/bookingForm/BookingForm.tsx
+++ src/assets/components/reservations/reservationHeader/bookingForm/BookingForm.tsx
@@ -16,50 +16,52 @@ const BookingForm: React.FC = () => {
     const refDateError = useRef<HTMLParagraphElement>(null);
     const refTimeError = useRef<HTMLParagraphElement>(null);
     return (
-        <div id="bookingForm">
-            <div id="nameLine" className='formLine'>
-                <input ref={refName} id="name" type='text' placeholder='Name'></input>
-                <p ref={refNameError} className='error'></p>
-            </div>
-            <div id="emailLine" className='formLine'>
-                <input ref={refEmail} id="email" type='text' placeholder='Email'></input>
-                <p ref={refEmailError} className='error'></p>
-            </div>
-            <div id="dateLine" className='formLine'>
-                <div>
-                    <p>Pick a date</p>
-                    <p ref={refDateError} className='error'></p>
+        <form id="bkForm">
+            <div id="bookingForm">
+                <div id="nameLine" className='formLine'>
+                    <input ref={refName} id="name" type='text' placeholder='Name'></input>
+                    <p ref={refNameError} className='error'></p>
                 </div>
-                <div>
-                    <input ref={refDay} id="day" className="shortInput" type='text' placeholder='DD'></input>
-                    <input ref={refMonth} id="month" className="shortInput" type='text' placeholder='MM'></input>
-                    <input ref={refYear} id="year" className="longInput" type='text' placeholder='YYYY'></input>
+                <div id="emailLine" className='formLine'>
+                    <input ref={refEmail} id="email" type='text' placeholder='Email'></input>
+                    <p ref={refEmailError} className='error'></p>
                 </div>
-            </div>
-            <div id="timeLine" className='formLine'>
-                <div>
-                    <p>Pick a time</p>
-                    <p ref={refTimeError} className='error'></p>
-                </div>
-                <div>
-                    <input ref={refHour} id="day" className="shortInput" type='text' placeholder='HH'></input>
-                    <input ref={refMinutes} id="month" className="shortInput" type='text' placeholder='MM'></input>
-                    <input ref={refDayTime} id="year" className="longInput" type='text' placeholder='AM'></input>
-                    <button id="dayTimeBt"></button>
-                    <div id="dayTimeselect">
-                        <button id="am-button" className='selectButton'>AM</button>
-                        <button id="pm-button" className='selectButton'>PM</button>
+                <div id="dateLine" className='formLine'>
+                    <div>
+                        <p>Pick a date</p>
+                        <p ref={refDateError} className='error'></p>
+                    </div>
+                    <div>
+                        <input ref={refDay} id="day" className="shortInput" type='text' placeholder='DD'></input>
+                        <input ref={refMonth} id="month" className="shortInput" type='text' placeholder='MM'></input>
+                        <input ref={refYear} id="year" className="longInput" type='text' placeholder='YYYY'></input>
                     </div>
                 </div>
-            </div>
+                <div id="timeLine" className='formLine'>
+                    <div>
+                        <p>Pick a time</p>
+                        <p ref={refTimeError} className='error'></p>
+                    </div>
+                    <div>
+                        <input ref={refHour} id="day" className="shortInput" type='text' placeholder='HH'></input>
+                        <input ref={refMinutes} id="month" className="shortInput" type='text' placeholder='MM'></input>
+                        <input ref={refDayTime} id="year" className="longInput" type='text' placeholder='AM'></input>
+                        <button id="dayTimeBt"></button>
+                        <div id="dayTimeselect">
+                            <button id="am-button" className='selectButton'>AM</button>
+                            <button id="pm-button" className='selectButton'>PM</button>
+                        </div>
+                    </div>
+                </div>
 
-            <div id="guestsLine" className='formLine'>
-                <button id="restPeopleBt" className='setPeopleBt'></button>
-                <p>4 people</p>
-                <button id="addPeopleBt" className='setPeopleBt'></button>
+                <div id="guestsLine" className='formLine'>
+                    <button id="restPeopleBt" className='setPeopleBt'></button>
+                    <p>4 people</p>
+                    <button id="addPeopleBt" className='setPeopleBt'></button>
+                </div>
+                <DefaultButton text='MAKE RESERVATION' buttonType='darkBt' />
             </div>
-            <DefaultButton text='MAKE RESERVATION' buttonType='darkBt' />
-        </div>
+        </form>
     )
 }
 export default BookingForm;
