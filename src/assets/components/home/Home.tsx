7e6ab461b50a9c3815f3194af5ac269898c2ab7a
Daniel Martinez Duque <danielopq@gmail.com>
2025-01-31 14:58:15 +0000

    Home.tsx - tablet version comments added

--- src/assets/components/home/Home.tsx
+++ src/assets/components/home/Home.tsx
@@ -1,5 +1,11 @@
 import './home.css';
 import { HomeHeader, EnjoyablePlace, LocallySourcedFood, Highlights, Slide, HomeFooter } from './components'
+
+/**
+ * Component that renders the home page of the website.
+ * 
+ * @returns {JSX.Element} - Home page of the website.
+ */
 const Home: React.FC = () => {
     return (
         <>
