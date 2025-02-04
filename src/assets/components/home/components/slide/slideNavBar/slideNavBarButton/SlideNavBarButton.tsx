b1e43ff19c46d0af496306b10a444fc62a97d890
Daniel Martinez Duque <danielopq@gmail.com>
2025-02-04 10:48:26 +0000

    SlideNavBarButton.tsx - comments added

--- src/assets/components/home/components/slide/slideNavBar/slideNavBarButton/SlideNavBarButton.tsx
+++ src/assets/components/home/components/slide/slideNavBar/slideNavBarButton/SlideNavBarButton.tsx
@@ -7,6 +7,17 @@ interface SlideNavBarButtonProps{
     slideEvent:number;
 }
 
+/**
+ * Renders a button in the slide navigation bar, allowing navigation through  
+ * the different special events offered by the restaurant.
+ * 
+ * @param param0 - Component props.
+ * @param {string} param0.text - The text displayed on the button.
+ * @param {boolean} param0.disabled - Indicates whether the button is disabled.
+ * @param {function} param0.setSlide - Function to update the displayed special event.
+ * @param {number} param0.slideEvent - The special event to be displayed.
+ * @returns A React component representing a navigation button.
+ */
 const SlideNavBarButton: React.FC<SlideNavBarButtonProps> = ({text,disabled,setSlide,slideEvent}) => {
     return (
         <div className='slideNavBarButton'>
