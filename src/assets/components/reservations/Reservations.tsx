c03e428bd13593a0b29bc638bf31d445ddbcdf6a
Daniel Martinez Duque <danielopq@gmail.com>
2025-02-06 09:22:21 +0000

    Reservations.tsx - comments added

--- src/assets/components/reservations/Reservations.tsx
+++ src/assets/components/reservations/Reservations.tsx
@@ -2,6 +2,11 @@ import './reservations.css';
 import FooterDetails from '../shared/footerDetails/FooterDetails';
 import ReservationsHeader from './reservationHeader/ReservationsHeader';
 
+/**
+ * Component that renders the reservations section of the website.
+ *
+ * @returns {JSX.Element} The Reservations component.
+ */
 const Reservations: React.FC = () => {
     return (
         <main id="reservations">
