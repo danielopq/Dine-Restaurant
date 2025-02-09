95de58edf2d7cdc29c2a5019a6ec301420624e60
Daniel Martinez Duque <danielopq@gmail.com>
2025-02-09 10:26:03 +0000

    Reservations section - mobile version done

--- src/assets/components/reservations/reservationHeader/ReservationsHeader.tsx
+++ src/assets/components/reservations/reservationHeader/ReservationsHeader.tsx
@@ -16,14 +16,14 @@ const ReservationsHeader: React.FC = (): JSX.Element => {
     }
     return (
         <header id="reservationsHeader">
-            {/* <div id="reservations-text">
+            <div id="reservations-text">
                 <h1>Reservations</h1>
                 <p className='mediumText'>
                     We can’t wait to host you. If you have any special requirements please feel
                     free to call on the phone number below. We’ll be happy to accommodate you.
                 </p>
                 <DefaultButton text='RESERVE PLACE' buttonStyle='lightBt' handleClick={handleClick}/>
-            </div> */}
+            </div>
             <BookingForm/>
         </header>
     )
