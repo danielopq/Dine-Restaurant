b1f6b8ca33eddbd62e562ed8d63c10511c654568
Daniel Martinez Duque <danielopq@gmail.com>
2025-02-14 11:52:50 +0000

    closeConfirmationWidow.ts - comments added

--- src/assets/components/reservations/reservationHeader/bookingForm/services/closeConfirmationWidow.ts
+++ src/assets/components/reservations/reservationHeader/bookingForm/services/closeConfirmationWidow.ts
@@ -1,3 +1,6 @@
+/**
+ * Closes the confirmation window of the registration form.
+ */
 export const closeConfirmationWidow = ():void=>{
     const confirmationWindow = document.getElementById('confirmationWindow') as HTMLDivElement | null;
     confirmationWindow && (confirmationWindow.style.display = 'none'); 
